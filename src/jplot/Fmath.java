4c6744d351cbd8a0ca50f818116925d58b3e97c9
bloc97 <blocbloc97@gmail.com>
2016-07-14 16:58:12 -0400

    Project Refresh

---
+++ src/jplot/Fmath.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jplot;
+
+/**
+ *
+ * @author Bowen
+ */
+public class Fmath {
+    
+    static double PI = 3.14159265;
+    
+    
+    public static double sin(double i){
+        double x=i%(2*PI);
+        double y;
+        
+        int v=1; int w=1;
+        if (x>PI) {x=x-PI;w=-1;}
+        if (x<-PI) {x=x+2*PI;}
+        if (x<0 && x>=-PI) {x=x+PI;v=-1;}
+        y=v*w*(16*x*(PI-x))/(5*PI*PI-4*x*(PI-x));
+
+        return y;
+    }
+    
+    public static double cos(double i){
+        double x = i+PI/2;
+        double y = sin(x);
+        return y;
+    }
+    
+    public static double fourier(double x, double h, int max){
+       // double a0=h/2;
+        double a0=0;
+        double sum=a0;
+        double bn;
+        for (int i=1; i<=max; i=i+2){
+            bn = (2*h)/(i*PI);
+            sum = sum + bn*Math.sin(x*i);
+        }
+        return sum*2;
+    }
+    
+    
+    
+}
---
+++ src/jplot/Main.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jplot;
+
+import java.util.Random;
+import java.awt.*;
+import java.util.Scanner;
+import javax.swing.*;
+
+/**
+ *
+ * @author Bowen
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private static JFrame initGUI(){
+        JFrame frame = new JFrame("Plotter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return frame;
+    }
+    
+    private static JPanel initPanel(){
+        JPanel content = new JPanel();
+        content.setOpaque(true);
+        content.setBackground(Color.WHITE);
+        content.setLayout(null);
+        return content;
+    }
+    
+    private static void sendGUI(int XSIZE, int YSIZE, double amp, double omega, double phi, int SCALE, double PRECISION, int SUM, int CHOICE, JPanel content){
+        
+        int XTRANS = XSIZE/2;
+        int YTRANS = YSIZE/2-YSIZE/12;
+        //double pi = Math.PI;
+        switch (CHOICE){
+            default:
+                Draw.drawSin(amp,omega,phi,XTRANS,YTRANS,PRECISION,SCALE,content);
+        break;
+            case 2:
+                Draw.drawFourier(amp,omega,phi,SUM,XTRANS,YTRANS,PRECISION,SCALE,content);
+        break;
+            case 3:
+                Draw.drawSin(amp,omega,phi,XTRANS,YTRANS,PRECISION,SCALE,content);
+                Draw.drawFourier(amp,omega,phi,SUM,XTRANS,YTRANS,PRECISION,SCALE,content);
+        break;
+        }
+        Draw.drawAxes(XTRANS,YTRANS,SCALE,PRECISION,content);
+        
+        Draw.drawLabels(XTRANS,YTRANS,PRECISION,SCALE,content);
+    }
+        
+    private static void displayGUI(JFrame frame, JPanel content, int XSIZE, int YSIZE){
+        frame.setContentPane(content);
+        frame.setSize(XSIZE, YSIZE);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+        
+    }
+    
+    public static void main(String... args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter 0 for automatic values, 1 to change function, 2 for manual settings: "); int MANUAL = input.nextInt(); System.out.print("");
+        
+        int XSIZE,YSIZE,SCALE,SUM,CHOICE;
+        double amp,omega,phi,PRECISION;
+        
+        
+        switch (MANUAL){
+            case 2:
+        System.out.print("Enter Horizontal Screen Size (Int): "); XSIZE = input.nextInt(); System.out.print("");
+        System.out.print("Enter Vertical Screen Size (Int): "); YSIZE = input.nextInt(); System.out.print("");
+        System.out.print("Enter Scale (Int): "); SCALE = input.nextInt(); System.out.println("");
+        System.out.print("Enter Amplitude: "); amp = input.nextDouble(); System.out.print("");
+        System.out.print("Enter Angular Frequency: "); omega = input.nextDouble(); System.out.print("");
+        System.out.print("Enter Phase Constant: "); phi = input.nextDouble(); System.out.print("");
+        System.out.print("Enter Step Function Precision: "); double iPRECISION = input.nextDouble(); System.out.print("");
+        if (iPRECISION<0.001){PRECISION=0.001;}
+        else{PRECISION=iPRECISION;}
+        System.out.print("Enter 1 for Sine Wave, 2 for Square Fourier Transform, 3 for both: "); CHOICE = input.nextInt(); System.out.print("");
+        if (CHOICE==2 || CHOICE==3){
+                System.out.print("Enter Sum Constant (Int): "); SUM = input.nextInt(); System.out.print("");
+            }
+        else{
+            SUM = 20;
+        }
+        break;
+            case 1:
+        XSIZE=800;YSIZE=600;SCALE=50;SUM=20;PRECISION=0.02;
+        System.out.print("Enter Amplitude: "); amp = input.nextDouble(); System.out.print("");
+        System.out.print("Enter Angular Frequency: "); omega = input.nextDouble(); System.out.print("");
+        System.out.print("Enter Phase Constant: "); phi = input.nextDouble(); System.out.print("");
+        System.out.print("Enter 1 for Sine Wave, 2 for Square Fourier Transform, 3 for both: "); CHOICE = input.nextInt(); System.out.print("");
+        break;
+            default:
+        XSIZE=800;YSIZE=600;SCALE=50;SUM=20;CHOICE=3;amp=1;omega=1;phi=0;PRECISION=0.02;
+        break;
+        }
+
+        
+//        SwingUtilities.invokeLater(new Runnable(){
+//            public void run()
+//            {
+        
+            JFrame frame = new Main().initGUI();      
+            JPanel content = new Main().initPanel();
+            Main.sendGUI(XSIZE,YSIZE,amp,omega, phi,SCALE,PRECISION,SUM,CHOICE,content);
+            Main.displayGUI(frame,content,XSIZE,YSIZE);
+        
+                
+//        }
+//    });
+        
+        }
+
+    
+}
+
